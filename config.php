dde326400b17057c88a99f9f161d959a3ae18909
arsyadfikom-sketch <arsyadfikom@gmail.com>
2026-05-27 22:15:43 +0800

    Update config.php

--- config.php
+++ config.php
@@ -8,9 +8,9 @@ use Aws\S3\S3Client;
 // DATABASE RDS
 // =========================
 
-$host = "ENDPOINT-RDS";
+$host = "siswa-db.c83ya4kmsi7u.us-east-1.rds.amazonaws.com";
 $user = "admin";
-$pass = "PASSWORD-RDS";
+$pass = "admin2026";
 $db   = "sekolah";
 
 $conn = mysqli_connect($host,$user,$pass,$db);
